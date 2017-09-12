c433e86c9b69c552971a3379da35321b260999e8
jalluriharsha <31794114+jalluriharsha@users.noreply.github.com>
2017-09-12 13:29:15 +0530

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student Students=new Students(550,"HARSHA","1997-04-15",75.52);
+	}
+
+}
